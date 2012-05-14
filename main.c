14ee58ecaae4cb07988facf89ce3407b6c14fd99
koren@pukogames.com <koren@pukogames.com@3857074f-9527-4ab4-1c64-02ed941a9e56>
2012-05-14 12:19:47 +0000

    git-svn-id: https://esgi-c-minidb.googlecode.com/svn/trunk@8 3857074f-9527-4ab4-1c64-02ed941a9e56

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include "lib/libcsv.c"
+#include <stdio>
 
 int main (void) {
-
+	//load all databases into memory
+	
+	//execute the console thing, where user can enter commands (while command != exit listen for another command)
+		//while loop
+			//parse command, execute it and retreive rows from the selected database
+	
+	//print result
 }
