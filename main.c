74b4809d8e1e045cc7ffcb6fbecc32529e3a1dc5
koren@pukogames.com <koren@pukogames.com@3857074f-9527-4ab4-1c64-02ed941a9e56>
2012-05-14 13:10:03 +0000

    git-svn-id: https://esgi-c-minidb.googlecode.com/svn/trunk@10 3857074f-9527-4ab4-1c64-02ed941a9e56

--- main.c
+++ main.c
@@ -1,12 +1,55 @@
 #include "lib/libcsv.c"
-#include <stdio>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+
+void load_db(char* db_name){
+	printf("Loading ");
+	printf(db_name);
+	printf("...\n");
+}
+
+int load_db_list(){
+	DIR* dirp = opendir("databases/");
+	int errno;
+	struct dirent* dp;
+	int db_count = 0;
+
+	while (dirp) {
+		errno = 0;
+		if ((dp = readdir(dirp)) != NULL && (strcmp(dp->d_name, ".svn") != 0) && (strcmp(dp->d_name, ".") != 0)) {
+			db_count++;
+			load_db(dp->d_name);
+		} else {
+			if (errno == 0) {
+				closedir(dirp);
+				return 0;
+			}
+			closedir(dirp);
+			return -1;
+		}
+	}
+	printf("\n");
+	return db_count;
+}
 
 int main (void) {
 	//load all databases into memory
-	
+	load_db_list();
+
 	//execute the console thing, where user can enter commands (while command != exit listen for another command)
-		//while loop
-			//parse command, execute it and retreive rows from the selected database
-	
-	//print result
+	char user_input[] = "";
+	while(strcmp(user_input, "quit") != 0 && strcmp(user_input, "exit") != 0){
+		printf(">");
+		//get command line
+		scanf("%s", user_input);
+		//parse the command line and do the sorting
+
+		//print the result
+	}
+
+	printf("	Column1		Column2		Column3\n");
+	printf("0:	Row1		Row2		Row3\n");
+	printf("Exit...\n");
 }
